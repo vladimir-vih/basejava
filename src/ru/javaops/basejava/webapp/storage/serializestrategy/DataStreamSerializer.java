125cef20c6f56e2697a3414d6dd0bb2055af41b2
vladimir-vih <marten3@yandex.ru>
2022-06-07 02:38:28 +0300

    Homework for the BaseJava lesson10 (version 6). All changes are made in the DataStreamSerializer file: - changed method "accept" of interfaces ThrowingObjectWriter and ThrowingObjectReader not to use DataOutputStream and DataInputStream - renamed "accept" to "write" in the ThrowingObjectWriter interface - renamed "accept" to "read" in the ThrowingObjectReader interface - replaced method "writeOptionalFieldFlag" by direct use of "writeBoolean" - replaced method "existOptionalField" by direct use of "readBoolean" - redesigned method "readMapWithException" to "readWithException" - redesigned method "sectionReader" to use "readWithException" method - removed "readListWithException" method

--- src/ru/javaops/basejava/webapp/storage/serializestrategy/DataStreamSerializer.java
+++ src/ru/javaops/basejava/webapp/storage/serializestrategy/DataStreamSerializer.java
@@ -13,9 +13,11 @@ public class DataStreamSerializer implements SerializeStrategy {
             dos.writeUTF(r.getUuid());
             dos.writeUTF(r.getFullName());
             Map<ContactType, String> contacts = r.getContacts();
-            writeWithException(contacts.entrySet(), this::writeContact, dos);
+            writeWithException(contacts.entrySet(),
+                    (Map.Entry<ContactType, String> entry) -> writeContact(entry, dos), dos);
             Map<SectionType, Section> sections = r.getSections();
-            writeWithException(sections.entrySet(), this::writeSection, dos);
+            writeWithException(sections.entrySet(),
+                    (Map.Entry<SectionType, Section> entry) -> writeSection(entry, dos), dos);
         }
     }
 
@@ -24,7 +26,7 @@ public class DataStreamSerializer implements SerializeStrategy {
         Objects.requireNonNull(collection);
         dos.writeInt(collection.size());
         for (T t : collection) {
-            objectWriter.accept(t, dos);
+            objectWriter.write(t);
         }
     }
 
@@ -46,12 +48,12 @@ public class DataStreamSerializer implements SerializeStrategy {
             case ACHIEVEMENT:
             case QUALIFICATIONS:
                 final Section<List<String>> skillsSection = (SkillsSection) section;
-                writeWithException(skillsSection.getBody(), (String s, DataOutputStream d) -> d.writeUTF(s), dos);
+                writeWithException(skillsSection.getBody(), dos::writeUTF, dos);
                 break;
             case EXPERIENCE:
             case EDUCATION:
                 final Section<List<Experience>> experienceSection = (ExperienceSection) section;
-                writeWithException(experienceSection.getBody(), this::writeExperience, dos);
+                writeWithException(experienceSection.getBody(), (Experience exp) -> writeExperience(exp, dos), dos);
                 break;
         }
     }
@@ -60,27 +62,19 @@ public class DataStreamSerializer implements SerializeStrategy {
         final Company company = experience.getCompany();
         dos.writeUTF(company.getName());
         final Link companyLink = company.getUrl();
-        if (writeOptionalFieldFlag(companyLink, dos)) writeLink(companyLink, dos);
+        dos.writeBoolean(companyLink != null);
+        if (companyLink != null) writeLink(companyLink, dos);
         dos.writeUTF(experience.getStartDate().toString());
         dos.writeUTF(experience.getEndDate().toString());
         dos.writeUTF(experience.getShortInfo());
         final String detailedInfo = experience.getDetailedInfo();
-        if (writeOptionalFieldFlag(detailedInfo, dos)) dos.writeUTF(detailedInfo);
+        dos.writeBoolean(detailedInfo != null);
+        if (detailedInfo != null) dos.writeUTF(detailedInfo);
     }
 
     private void writeLink(Link link, DataOutputStream dos) throws IOException {
         dos.writeUTF(link.getName());
         dos.writeUTF(link.getUrl());
-    }
-
-    private <T> boolean writeOptionalFieldFlag(T object, DataOutputStream dos) throws IOException {
-        if (object != null) {
-            dos.writeBoolean(true);
-            return true;
-        } else {
-            dos.writeBoolean(false);
-            return false;
-        }
     }
 
     @Override
@@ -91,25 +85,21 @@ public class DataStreamSerializer implements SerializeStrategy {
             Resume resume = new Resume(uuid, fullname);
 
             Map<ContactType, String> contacts = new EnumMap<>(ContactType.class);
-            readMapWithException(contacts, (Map m, DataInputStream d) -> this.contactsReader(m, d), dis);
+            readWithException(() -> contacts.put(ContactType.valueOf(dis.readUTF()), dis.readUTF()), dis);
             resume.setContacts(contacts);
 
             Map<SectionType, Section> sections = new EnumMap<>(SectionType.class);
-            readMapWithException(sections, (Map m, DataInputStream d) -> this.sectionReader(m, d), dis);
+            readWithException(() -> sectionReader(sections,dis), dis);
             resume.setSections(sections);
             return resume;
         }
     }
 
-    private <T extends Map> void readMapWithException(T map, ThrowingObjectReader<T> objectReader,
-                                                      DataInputStream dis) throws IOException {
+    private void readWithException(ThrowingObjectReader objectReader,
+                                   DataInputStream dis) throws IOException {
         for (int amount = dis.readInt(); amount > 0; amount--) {
-            objectReader.accept(map, dis);
+            objectReader.read();
         }
-    }
-
-    private void contactsReader(Map map, DataInputStream dis) throws IOException {
-        map.put(ContactType.valueOf(dis.readUTF()), dis.readUTF());
     }
 
     private void sectionReader(Map map, DataInputStream dis) throws IOException {
@@ -123,49 +113,38 @@ public class DataStreamSerializer implements SerializeStrategy {
             case ACHIEVEMENT:
             case QUALIFICATIONS:
                 final List<String> skillsList = new ArrayList<>();
-                readListWithException(skillsList, (List l, DataInputStream d) -> l.add(dis.readUTF()), dis);
+                readWithException(() -> skillsList.add(dis.readUTF()), dis);
                 map.put(sectionType, new SkillsSection(skillsList));
                 break;
             case EXPERIENCE:
             case EDUCATION:
                 final List<Experience> experienceList = new ArrayList<>();
-                readListWithException(experienceList, (List l, DataInputStream d) -> l.add(readExperience(d)), dis);
+                readWithException(() -> experienceList.add(readExperience(dis)), dis);
                 map.put(sectionType, new ExperienceSection(experienceList));
                 break;
         }
     }
 
-    private <T extends List> void readListWithException(T list, ThrowingObjectReader<T> objectReader,
-                                                        DataInputStream dis) throws IOException {
-        for (int i = dis.readInt(); i > 0 ; i--) {
-            objectReader.accept(list, dis);
-        }
-    }
-
     private Experience readExperience(DataInputStream dis) throws IOException {
         String companyName = dis.readUTF();
-        Link companyLink = existOptionalField(dis) ? readLink(dis) : null;
+        Link companyLink = dis.readBoolean() ? readLink(dis) : null;
         final Company company = new Company(companyName, companyLink);
         final LocalDate startDate = LocalDate.parse(dis.readUTF());
         final LocalDate endDate = LocalDate.parse(dis.readUTF());
         final String shortInfo = dis.readUTF();
-        final String detailedInfo = existOptionalField(dis) ? dis.readUTF() : null;
+        final String detailedInfo = dis.readBoolean() ? dis.readUTF() : null;
         return new Experience(company, startDate, endDate, shortInfo, detailedInfo);
     }
 
     private Link readLink(DataInputStream dis) throws IOException {
         return new Link(dis.readUTF(), dis.readUTF());
     }
-
-    private <T> boolean existOptionalField(DataInputStream dis) throws IOException {
-        return dis.readBoolean();
-    }
 }
 
 interface ThrowingObjectWriter<T> {
-    void accept(T t, DataOutputStream dos) throws IOException;
+    void write(T t) throws IOException;
 }
 
-interface ThrowingObjectReader<T> {
-    void accept(T t, DataInputStream dis) throws IOException;
+interface ThrowingObjectReader {
+    void read() throws IOException;
 }
